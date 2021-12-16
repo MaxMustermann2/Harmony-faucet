8dec3c0a9e245752434ff1b6e806f1791f9a4ca3
P-OPS <61007546+pops-one@users.noreply.github.com>
2021-12-16 10:51:36 +0400

    fix for faucet hang issue

--- faucet-backend/index.js
+++ faucet-backend/index.js
@@ -1,6 +1,11 @@
 import app from "./server.js";
 import { port } from "./constants.js";
 
+// To catch the unhandledRejection in app
+process.on('unhandledRejection', (err) => {
+  console.error("UnhandleRejection", err.message);
+})
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
 });
